a0a0ce2702cd6269c4df3755818632284d45b67d
Hudson Souza <hudsonss@gmail.com>
2015-01-27 00:43:45 -0200

    Meu primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?
+	$distro = "Ubuntu 14.10";
+	$usuario = "Hudson Souza";
+	$email = "hudsonss@gmail.com";
+	$hoje = date('d-m-Y H:i:s');
+?>
+
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8; true">
+	<title>HuDSON</title>
+</head>
+
+<body>
+	<center>
+	<br>
+	<h1>HuDSON :-)</h1>
+	<br>
+	<br>Distribuição...: <?=$distro?>
+	<br>Usuário...: <?=$usuario?>
+	<br>E-mail...: <?=$email?>
+	<br>Data...: <?=$hoje?>
+	</center>
+</body>
+
+</html>
+
+
+
+
+
